d0719e6310189c8ba16983118c8ed7888d5294d0
Courtney Robinson <courtney@crlog.info>
2012-01-12 17:28:20 +0000

    added date

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -3,4 +3,4 @@ print_r($query1);
 print_r($query2);
 print_r($query3);
 ?>
-<p><br />Page rendered in {elapsed_time} seconds</p>
+<p><br />Page rendered in {elapsed_time} seconds <?php echo date('l jS \of F Y h:i:s A'); ?></p>
